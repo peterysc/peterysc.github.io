f2046161dda9cdda934f98e6cedd77dc6bd029d9
peterysc <peterysc@gmail.com>
2017-05-20 17:52:17 -0700

    little bit of material design and refactor in js

--- js/index.js
+++ js/index.js
@@ -4,12 +4,15 @@ $("#resumeDiv").hover(displayText, hideText);
 $("#contactDiv").hover(displayText, hideText);
 
 function displayText(){
-	var textChild = $(this).children().attr("id");
-	$("#"+textChild).css('visibility','visible')
+	var textChild = $(this).children();
+	var textChildId = textChild.attr("id");
+	$("#"+textChildId).css('visibility','visible');
+
 	// $(".actualText").css('opacity','100 !important')
 }
 
 function hideText(){
-	var textChild = $(this).children().attr("id");
-	$("#"+textChild).css('visibility','hidden')
+	var textChild = $(this).children();
+	var textChildId = textChild.attr("id");
+	$("#"+textChildId).css('visibility','hidden')
 }
