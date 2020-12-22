3b42b13a178fe363ae1807711a3a17748bab590f
Peter Chung <peterysc@gmail.com>
2020-12-21 22:38:13 -0800

    Adding personal project section

--- js/index.js
+++ js/index.js
@@ -1,37 +1,38 @@
-$("#profileDiv").hover(displayText, hideText);
-$("#portfolioDiv").hover(displayText, hideText);
-$("#resumeDiv").hover(displayText, hideText);
-$("#contactDiv").hover(displayText, hideText);
-
-function displayText(){
-	var textChild = $(this).children();
-	var textChildId = textChild.attr("id");
-	$("#"+textChildId).css('visibility','visible');
-
-	// $(".actualText").css('opacity','100 !important')
-}
-
-function hideText(){
-	var textChild = $(this).children();
-	var textChildId = textChild.attr("id");
-	$("#"+textChildId).css('visibility','hidden')
-}
-
-$("#linkedInAddr").tooltip({
-	show: {
-        effect: "slideDown",
-        delay: 250
-    }
-})
-
-$("#githubAddr").tooltip({
-	show: {
-        effect: "slideDown",
-        delay: 250
-    }
-})
-
-$("#profileText").click(function(){
-	console.log("clicked")
-	$("#myModal").modal('show');
+$("#moveToExp").on('click', function(e) {
+	e.preventDefault();
+	scrollSpyToSection("#expSection");
 });
+
+$("#moveToPersonalProject").on('click', function(e) {
+	e.preventDefault();
+	scrollSpyToSection("#personalProjectSection");
+});
+
+$("#moveToContact").on('click', function(e) {
+	e.preventDefault();
+	scrollSpyToSection("#contactSection");
+});
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+		
+	// scroll body to 0px on click
+	$('#back-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+			}, 1000);
+		return false;
+	});
+});
+
+function scrollSpyToSection(section){
+	$('html, body').animate({
+		scrollTop: $(section).offset().top
+	}, 1000);
+}
